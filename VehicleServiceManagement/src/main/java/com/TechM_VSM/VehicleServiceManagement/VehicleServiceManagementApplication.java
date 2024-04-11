022a540b58d6b17d0bf3ddf7ac0eae5893f83db4
abhisheknandre <abhinandre01@gmail.com>
2024-04-12 00:55:26 +0530

    first commit

---
+++ VehicleServiceManagement/src/main/java/com/TechM_VSM/VehicleServiceManagement/VehicleServiceManagementApplication.java
@@ -0,0 +1,13 @@
+package com.TechM_VSM.VehicleServiceManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleServiceManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleServiceManagementApplication.class, args);
+	}
+
+}
