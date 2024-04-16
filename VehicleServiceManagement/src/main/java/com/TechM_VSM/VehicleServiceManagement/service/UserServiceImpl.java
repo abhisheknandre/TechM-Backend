b863be05d09c72de01d29da417cfbc526578e6ac
nikhilnc1 <nikhilnchavan1@gmail.com>
2024-04-17 01:26:27 +0530

    JWT implemented

--- VehicleServiceManagement/src/main/java/com/TechM_VSM/VehicleServiceManagement/service/UserServiceImpl.java
+++ VehicleServiceManagement/src/main/java/com/TechM_VSM/VehicleServiceManagement/service/UserServiceImpl.java
@@ -6,6 +6,7 @@ import com.TechM_VSM.VehicleServiceManagement.model.User;
 import com.TechM_VSM.VehicleServiceManagement.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
