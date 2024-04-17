6e1c817983d1ea722eab6ed26eea6967eb57f76b
nikhilnc1 <nikhilnchavan1@gmail.com>
2024-04-17 12:59:33 +0530

    ItemService API added

--- VehicleServiceManagement/src/main/java/com/TechM_VSM/VehicleServiceManagement/service/ItemService.java
+++ VehicleServiceManagement/src/main/java/com/TechM_VSM/VehicleServiceManagement/service/ItemService.java
@@ -1,7 +1,19 @@
 package com.TechM_VSM.VehicleServiceManagement.service;
 
 import com.TechM_VSM.VehicleServiceManagement.dto.ItemDto;
+import com.TechM_VSM.VehicleServiceManagement.model.Item;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+import java.util.Map;
 
 public interface ItemService {
     ItemDto saveVehicle(ItemDto itemDto);
+
+    ResponseEntity<List<Item>> getAll();
+
+
+    Map<String, Boolean> deleteItem(int itemId);
+
+    ResponseEntity<Item> getitemById(int id);
 }
